59a6478f40e94d19941f16b486e86c00bc0597bc
JYB0 <byunjunyoung1@naver.com>
2025-01-03 11:46:40 +0900

    workout_stopwatch_UI_edit

--- lib/screens/workout_screen.dart
+++ lib/screens/workout_screen.dart
@@ -44,7 +44,7 @@ class WorkoutScreenState extends State<WorkoutScreen> {
       sets = [];
       restTime = 60; // 기본값
     }
-    setState(() {}); // 화면 갱신
+    setState(() {});
   }
 
   void _startRestTimer() {
@@ -66,74 +66,6 @@ class WorkoutScreenState extends State<WorkoutScreen> {
         _showRestCompleteNotification();
       }
     });
-
-    // Show the bottom sheet for visual effect
-    _showRestBottomSheet();
-  }
-
-  void _showRestBottomSheet() {
-    showModalBottomSheet(
-      context: context,
-      isDismissible: false,
-      enableDrag: false,
-      builder: (context) {
-        return StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState) {
-            return Container(
-              color: Colors.white,
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    "휴식 중...",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 10),
-                  StreamBuilder<int>(
-                    stream: Stream.periodic(
-                        const Duration(seconds: 1), (tick) => elapsedSeconds),
-                    builder: (context, snapshot) {
-                      final currentTime = snapshot.data ?? elapsedSeconds;
-                      return Column(
-                        children: [
-                          Text(
-                            "${currentTime ~/ 60}:${(currentTime % 60).toString().padLeft(2, '0')}",
-                            style: const TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 10),
-                          LinearProgressIndicator(
-                            value: currentTime <= restTime
-                                ? currentTime / restTime
-                                : 1.0, // restTime 경과 후에도 프로그레스바 유지
-                            backgroundColor: Colors.grey[300],
-                            color: currentTime <= restTime
-                                ? Colors.red
-                                : Colors.green, // restTime 경과 후 색상 변경
-                            minHeight: 10,
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context); // Close the bottom sheet
-                      setState(() {
-                        isResting = false;
-                      });
-                    },
-                    child: const Text("휴식 종료"),
-                  ),
-                ],
-              ),
-            );
-          },
-        );
-      },
-    );
   }
 
   void _showRestCompleteNotification() async {
@@ -215,30 +147,107 @@ class WorkoutScreenState extends State<WorkoutScreen> {
       appBar: AppBar(
         title: Text("Week ${widget.week}, Day ${widget.day}"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Text(
-                "세트 ${currentSet + 1} / ${sets.length}",
-                style:
-                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                "현재 세트 푸시업: ${sets.isNotEmpty ? sets[currentSet] : 0}개",
-                style: const TextStyle(fontSize: 18),
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: _completeSet,
-                child: Text(
-                  currentSet < sets.length - 1 ? "세트 완료" : "운동 완료",
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      "세트 ${currentSet + 1} / ${sets.length}",
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      "현재 세트 푸시업: ${sets.isNotEmpty ? sets[currentSet] : 0}개",
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    const SizedBox(height: 40),
+                    ElevatedButton(
+                      onPressed: _completeSet,
+                      child: Text(
+                        currentSet < sets.length - 1 ? "세트 완료" : "운동 완료",
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+            if (isResting)
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16.0),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color.fromARGB(25, 0, 0, 0),
+                          blurRadius: 10.0,
+                          offset: Offset(0, -2),
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // 스탑워치와 휴식 중 텍스트
+                            Row(
+                              children: [
+                                Text(
+                                  "${elapsedSeconds ~/ 60}:${(elapsedSeconds % 60).toString().padLeft(2, '0')}",
+                                  style: const TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                const Text(
+                                  "휴식 중...",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            // 'X' 버튼
+                            IconButton(
+                              icon: const Icon(Icons.close),
+                              onPressed: () {
+                                setState(() {
+                                  isResting = false; // 창이 안 보이도록 설정
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        LinearProgressIndicator(
+                          value: elapsedSeconds / restTime,
+                          backgroundColor: Colors.grey[300],
+                          color: elapsedSeconds <= restTime
+                              ? Colors.red
+                              : Colors.green,
+                          minHeight: 10,
+                        ),
+                        const SizedBox(height: 10),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+          ],
         ),
       ),
     );
