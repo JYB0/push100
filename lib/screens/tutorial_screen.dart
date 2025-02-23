8b2f163b01ce615a6646ab9c60eea8f985b47495
JYB0 <byunjunyoung1@naver.com>
2025-02-23 12:26:56 +0900

    tutorial_screen_text_edit

--- lib/screens/tutorial_screen.dart
+++ lib/screens/tutorial_screen.dart
@@ -26,25 +26,43 @@ class TutorialScreen extends StatelessWidget {
               ),
             ),
             Text(
-              "2. 푸시업을 한 날로부터 2일 후에 푸시업(혹은 테스트)을 시작해 주세요.",
+              "2. 한 세트를 완료한 후, 노란색 원을 터치하면 해당 세트가 완료됩니다.",
               style: TextStyle(
                 fontSize: dynamicFontSize,
               ),
             ),
             Text(
-              "3. 테스트를 한 날로부터 2일 후에 푸시업을 진행해 주세요.",
+              "3. 정해진 개수를 채우지 못했다면 '-' 버튼을 눌러 수행한 횟수를 조정한 후, 세트를 완료하세요.",
               style: TextStyle(
                 fontSize: dynamicFontSize,
               ),
             ),
             Text(
-              "4. 테스트 통과 기준에 못 미치면 해당 주차는 반복됩니다.",
+              "4. 마지막 세트에서는 최대한 많은 푸시업을 수행하세요. (목표 횟수 이상 가능)",
               style: TextStyle(
                 fontSize: dynamicFontSize,
               ),
             ),
             Text(
-              "5. 6주간 프로그램 진행 후 푸시업 100개를 성공하면 끝입니다! 축하드립니다!",
+              "5. 푸시업을 수행한 후 2일 뒤 다시 훈련(또는 테스트)을 진행하세요.",
+              style: TextStyle(
+                fontSize: dynamicFontSize,
+              ),
+            ),
+            Text(
+              "6. 테스트를 진행한 후 2일 뒤 푸시업 훈련을 시작하세요.",
+              style: TextStyle(
+                fontSize: dynamicFontSize,
+              ),
+            ),
+            Text(
+              "7. 테스트 통과 기준에 미치지 못하면 해당 주차가 반복됩니다.",
+              style: TextStyle(
+                fontSize: dynamicFontSize,
+              ),
+            ),
+            Text(
+              "8. 6주간 프로그램을 완료하고 푸시업 100개를 달성하면 챌린지가 끝납니다! 🎉 축하합니다!",
               style: TextStyle(
                 fontSize: dynamicFontSize,
               ),
